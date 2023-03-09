35b9eff2d2984f89224b968b9a83456af819e010
Aftermath001 <alvinasiachi@gmail.com>
2023-03-09 08:33:09 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
+// const gifts = [ "teddy bear", "drone", "doll"];
+//  function wrapGifts(gift) {
+//     console.log (`Wrapped ${gift} and added a bow`);
+//  }
+//  wrapGifts(gifts [0]);
+//  wrapGifts(gifts [1]);
+//  wrapGifts(gifts [2]);
 function introduction (name){
-    return `Hi, my name is ${name}.`;
+ return `Hi, my name is ${name}.`;
 }
 function introductionWithLanguage(name, language){
-    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
-  }
-  function introductionWithLanguageOptional(name, language = "Javascript"){
-    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
-  }
-  
+   return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+function introductionWithLanguageOptional(name, language="JavaScript"){
+   return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
